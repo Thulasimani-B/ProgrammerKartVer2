44ebd157b5d6cc6fa73e7cfe6eb77f2ab8c45440
aravindh0799 <aravindhan0799@gmail.com>
2022-12-21 17:56:54 +0530

    registered orderplaced and payment models in admin
    Co-authored-by: Thulasimani-B <Thulasimani-B@users.noreply.github.com>

--- ProgrammersKartProject/proKApp/admin.py
+++ ProgrammersKartProject/proKApp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # importing all models
-from . models import Product,Customer,Cart
+from . models import Product,Customer,Cart, Payment, OrderPlaced
 
 
 
@@ -16,3 +16,11 @@ class CustomerModelAdmin(admin.ModelAdmin):
 @admin.register(Cart)
 class CartModelAdmin(admin.ModelAdmin):
     list_display = ['id','user','product','quantity']
+
+@admin.register(Payment)
+class PaymentModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','amount','razorpay_order_id','razorpay_payment_status','razorpay_payment_id','paid']
+
+@admin.register(OrderPlaced)
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','Customer','product','quantity','ordered_date','status','payment']
